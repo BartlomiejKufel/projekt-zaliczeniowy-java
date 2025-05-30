f69e30b04f128c7ebc41d5eda52c86c569d59e54
Bartłomiej Kufel <joloczok123@gmail.com>
2025-05-30 18:52:03 +0200

    wizualizacja setting paga

--- crowdle/src/main/java/com/crowdle/controller/SettingsPageController.java
+++ crowdle/src/main/java/com/crowdle/controller/SettingsPageController.java
@@ -1,8 +1,12 @@
 package com.crowdle.controller;
 
+import com.crowdle.utility.PageMenagerUtility;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -11,10 +15,18 @@ public class SettingsPageController {
 
 
     @FXML public ImageView iconImageView;
+    @FXML public Button backButton;
 
     @FXML
     public void initialize() throws FileNotFoundException {
         Image iconImg = new Image(new FileInputStream("images/icon_crowdle.png"));
         iconImageView.setImage(iconImg);
+    }
+
+
+    public void backButtonClick(ActionEvent actionEvent) {
+        Stage stage = (Stage) backButton.getScene().getWindow();
+        PageMenagerUtility.goToStartPage(stage);
+
     }
 }
